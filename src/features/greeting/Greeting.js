cb694345cb54937a10a2de71e6b3d1e43c8d4c68
Paul Onchera <hesbonpoisson@gmail.com>
2023-01-12 16:54:40 +0300

    Fetch greetings from the API

--- src/features/greeting/Greeting.js
+++ src/features/greeting/Greeting.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { getGreetings } from './greetingSlice';
 export function Greeting() {
-
+const dispatch = useDispatch()
+const data = useSelector(state => state.greeting)
+const { pending } = data
+useEffect(() => {
+  if (pending){
+    dispatch(getGreetings())
+  }
+}, [dispatch, pending])
   return (
     <div>
       <h1>Greetings</h1>
+      <div>{data.greetings.map(greeting =>{
+        return <p key={greeting.id}>{greeting.title}</p>
+      })}</div>
     </div>
   );
 }
