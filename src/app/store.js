6ee006d15f3d2a6027e7f741fe8e81fb3bdbdde5
Paul Onchera <hesbonpoisson@gmail.com>
2023-01-12 12:38:38 +0300

    Remove boilerplate code

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import greetingReducer from '../features/greeting/greetingSlice'
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    greeting: greetingReducer,
   },
 });
---
+++ src/features/greeting/Greeting.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+export function Greeting() {
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+    </div>
+  );
+}
